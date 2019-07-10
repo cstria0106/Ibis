6e1ed8ac1cd65ae4da5949d89ca5693dc9c05bd2
Koati <cstria0106@gmail.com>
2019-07-11 02:53:09 +0900

    Queue 클래스 생성자 수정

--- music/queue.js
+++ music/queue.js
@@ -2,22 +2,27 @@ const Discord = require('discord.js');
 const Music = require('./music');
 
 class Queue {
-    /**
-     * 
-     * @param {Discord.TextChannel} textChannel 
-     * @param {Discord.VoiceChannel} voiceChannel
-     * @param {Discord.Guild} guild
-     */
-    constructor(textChannel, voiceChannel, guild) {
-        this.textChannel = textChannel;
-        this.voiceChannel = voiceChannel;
-        this.guild = guild;
-
+    constructor() {
         this.playing = false;
         this.connected = false;
 
         this.repeat = false;
         this.shuffle = false;
+
+        /**
+         * @type {Discord.TextChannel}
+         */
+        this.textChannel = null;
+
+        /**
+         * @type {Discord.VoiceChannel}
+         */
+        this.voiceChannel = null;
+
+        /**
+         * @type {Discord.Guild}
+         */
+        this.guild = null;
 
         /**
          * @type {Music[]}
